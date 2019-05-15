9455bb0261ff6286dd764503e50b9f60d07f1194
jayaimzzz <jameshorton@gmail.com>
2019-05-15 16:46:39 -0400

    tweets are displaying on homepage

--- twitterclone/twitterusers/views.py
+++ twitterclone/twitterusers/views.py
@@ -6,5 +6,6 @@ from twitterclone.tweets.models import Tweet
 @login_required()
 def index_view(request):
     tweets = Tweet.objects.all()
+    sorted_tweets = sorted(tweets,key=lambda tweet: tweet.created, reverse=True)
     html = 'index.html'
-    return render(request,html)
+    return render(request,html, {'tweets': sorted_tweets})
